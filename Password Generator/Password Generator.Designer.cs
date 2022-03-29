9f9c3bb657e1f8d8b8d766757f0d82e2a08dab26
Andrea <Andrea@DESKTOP-BDOFMVL>
2022-03-29 16:07:25 +0200

    VERSIONE 1.0

--- Password Generator/Password Generator.Designer.cs
+++ Password Generator/Password Generator.Designer.cs
@@ -36,11 +36,16 @@
             this.lblPasswordOttenuta = new System.Windows.Forms.Label();
             this.lblRisultatoPassword = new System.Windows.Forms.Label();
             this.btnSalvaPassword = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblNomeSito
             // 
             this.lblNomeSito.AutoSize = true;
+            this.lblNomeSito.ForeColor = System.Drawing.Color.LightGreen;
             this.lblNomeSito.Location = new System.Drawing.Point(299, 31);
             this.lblNomeSito.Name = "lblNomeSito";
             this.lblNomeSito.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -50,6 +55,8 @@
             // 
             // txtboxNomeSito
             // 
+            this.txtboxNomeSito.BackColor = System.Drawing.Color.Black;
+            this.txtboxNomeSito.ForeColor = System.Drawing.Color.LightGreen;
             this.txtboxNomeSito.Location = new System.Drawing.Point(302, 62);
             this.txtboxNomeSito.Name = "txtboxNomeSito";
             this.txtboxNomeSito.Size = new System.Drawing.Size(100, 20);
@@ -59,6 +66,7 @@
             // lblNomeUtente
             // 
             this.lblNomeUtente.AutoSize = true;
+            this.lblNomeUtente.ForeColor = System.Drawing.Color.LightGreen;
             this.lblNomeUtente.Location = new System.Drawing.Point(299, 109);
             this.lblNomeUtente.Name = "lblNomeUtente";
             this.lblNomeUtente.Size = new System.Drawing.Size(103, 13);
@@ -68,6 +76,8 @@
             // 
             // txtboxNomeUtente
             // 
+            this.txtboxNomeUtente.BackColor = System.Drawing.Color.Black;
+            this.txtboxNomeUtente.ForeColor = System.Drawing.Color.LightGreen;
             this.txtboxNomeUtente.Location = new System.Drawing.Point(302, 145);
             this.txtboxNomeUtente.Name = "txtboxNomeUtente";
             this.txtboxNomeUtente.Size = new System.Drawing.Size(100, 20);
@@ -75,17 +85,21 @@
             // 
             // btnGeneraPassword
             // 
+            this.btnGeneraPassword.BackColor = System.Drawing.Color.Black;
+            this.btnGeneraPassword.ForeColor = System.Drawing.Color.LightGreen;
             this.btnGeneraPassword.Location = new System.Drawing.Point(298, 212);
             this.btnGeneraPassword.Name = "btnGeneraPassword";
             this.btnGeneraPassword.Size = new System.Drawing.Size(114, 23);
             this.btnGeneraPassword.TabIndex = 4;
             this.btnGeneraPassword.Text = "Genera Password";
-            this.btnGeneraPassword.UseVisualStyleBackColor = true;
+            this.btnGeneraPassword.UseVisualStyleBackColor = false;
+            this.btnGeneraPassword.Click += new System.EventHandler(this.btnGeneraPassword_Click);
             // 
             // lblPasswordOttenuta
             // 
             this.lblPasswordOttenuta.AutoSize = true;
-            this.lblPasswordOttenuta.Location = new System.Drawing.Point(295, 272);
+            this.lblPasswordOttenuta.ForeColor = System.Drawing.Color.LightGreen;
+            this.lblPasswordOttenuta.Location = new System.Drawing.Point(299, 267);
             this.lblPasswordOttenuta.Name = "lblPasswordOttenuta";
             this.lblPasswordOttenuta.Size = new System.Drawing.Size(115, 13);
             this.lblPasswordOttenuta.TabIndex = 5;
@@ -95,25 +109,62 @@
             // lblRisultatoPassword
             // 
             this.lblRisultatoPassword.AutoSize = true;
-            this.lblRisultatoPassword.Location = new System.Drawing.Point(329, 306);
+            this.lblRisultatoPassword.ForeColor = System.Drawing.Color.LightGreen;
+            this.lblRisultatoPassword.Location = new System.Drawing.Point(271, 305);
             this.lblRisultatoPassword.Name = "lblRisultatoPassword";
             this.lblRisultatoPassword.Size = new System.Drawing.Size(35, 13);
             this.lblRisultatoPassword.TabIndex = 6;
             this.lblRisultatoPassword.Text = "label4";
+            this.lblRisultatoPassword.Visible = false;
             // 
             // btnSalvaPassword
             // 
-            this.btnSalvaPassword.Location = new System.Drawing.Point(271, 344);
+            this.btnSalvaPassword.BackColor = System.Drawing.Color.Black;
+            this.btnSalvaPassword.ForeColor = System.Drawing.Color.LightGreen;
+            this.btnSalvaPassword.Location = new System.Drawing.Point(285, 339);
             this.btnSalvaPassword.Name = "btnSalvaPassword";
-            this.btnSalvaPassword.Size = new System.Drawing.Size(181, 23);
+            this.btnSalvaPassword.Size = new System.Drawing.Size(148, 23);
             this.btnSalvaPassword.TabIndex = 7;
             this.btnSalvaPassword.Text = "Salva password in locale";
-            this.btnSalvaPassword.UseVisualStyleBackColor = true;
+            this.btnSalvaPassword.UseVisualStyleBackColor = false;
+            this.btnSalvaPassword.Click += new System.EventHandler(this.btnSalvaPassword_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+
+            //
+            // pathFileSalvato
+            //
+            this.pathFileSalvato = @"E:\Password\password.txt";
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnSalvaPassword);
             this.Controls.Add(this.lblRisultatoPassword);
@@ -123,8 +174,13 @@
             this.Controls.Add(this.lblNomeUtente);
             this.Controls.Add(this.txtboxNomeSito);
             this.Controls.Add(this.lblNomeSito);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.OliveDrab;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Password Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +196,9 @@
         private System.Windows.Forms.Label lblPasswordOttenuta;
         private System.Windows.Forms.Label lblRisultatoPassword;
         private System.Windows.Forms.Button btnSalvaPassword;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
---
+++ Password Generator/Password Generator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+
+        private List<String> stringaDaSalvare;
+        private String pathFileSalvato;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGeneraPassword_Click(object sender, EventArgs e)
+        {
+            if (lblNomeSito != null && lblNomeUtente != null && !lblNomeSito.Text.Equals("") && !lblNomeUtente.Text.Equals(""))
+            {
+                lblRisultatoPassword.Text = GeneraPassword();
+                if (!lblRisultatoPassword.Text.Equals(""))
+                {
+                    lblRisultatoPassword.Visible = true;
+                }
+            }
+        }
+
+        private void btnSalvaPassword_Click(object sender, EventArgs e)
+        {
+            if (lblRisultatoPassword != null && lblRisultatoPassword.Visible && !lblRisultatoPassword.Equals(""))
+            {
+                SalvaPassword();
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Password Generator made by Andrea Antonio Perrelli." + System.Environment.NewLine + 
+                "Check https://github.com/AndreaPerrelli/Password-Generator for further updates.", "About");
+
+        }
+
+        private String GeneraPassword()
+        {
+            Random rnd = new Random();
+            List<String> numeri = new List<String> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            List<String> lettereAlfabetoMinuscole = new List<String> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "j", "y", "z", "k" };
+            List<String> lettereAlfabetoMaisucole = new List<String> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "J", "Y", "Z", "K" };
+            List<String> simboli = new List<String> { "#", "@", "]", "[", ",", ".", "-", "+", "*", "!", "£", "$", "%", "/", "(", ")", "=", "?", "^", "^" };
+            int length = 32;
+            String passwordGenerata = "";
+            int sceltaListe = 1;
+            int sceltaElementoLista = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                switch (sceltaListe)
+                {
+                    case 1:
+                        sceltaElementoLista = rnd.Next(numeri.Count);
+                        passwordGenerata += numeri[sceltaElementoLista];
+                        sceltaListe++;
+                        break;
+                    case 2:
+                        sceltaElementoLista = rnd.Next(lettereAlfabetoMinuscole.Count);
+                        passwordGenerata += lettereAlfabetoMinuscole[sceltaElementoLista];
+                        sceltaListe++;
+                        break;
+                    case 3:
+                        sceltaElementoLista = rnd.Next(lettereAlfabetoMaisucole.Count);
+                        passwordGenerata += lettereAlfabetoMaisucole[sceltaElementoLista];
+                        sceltaListe++;
+                        break;
+                    case 4:
+                        sceltaElementoLista = rnd.Next(simboli.Count);
+                        passwordGenerata += simboli[sceltaElementoLista];
+                        sceltaListe = 1;
+                        break;
+                    default:
+                        sceltaListe = 1;
+                        break;
+
+                }
+
+            }
+
+            stringaDaSalvare = new List<String> { "Nome Sito " + txtboxNomeSito.Text, 
+                                                  "Nome Utente/Email " + txtboxNomeUtente.Text, 
+                                                  "Password Generata " + passwordGenerata };
+
+
+            return passwordGenerata;
+        }
+        private void SalvaPassword()
+        {
+            if(!File.Exists(pathFileSalvato))
+            {
+                File.Create(pathFileSalvato).Dispose();
+                File.AppendAllText(pathFileSalvato, Environment.NewLine);
+                foreach (var stringa in stringaDaSalvare)
+                {
+                    File.AppendAllText(pathFileSalvato, stringa + Environment.NewLine);
+                }
+
+                MessageBox.Show("Salvataggio password avvenuto con successo, path : " + pathFileSalvato, "successo");
+            } else
+            {
+                File.AppendAllText(pathFileSalvato, Environment.NewLine);
+                foreach (var stringa in stringaDaSalvare)
+                {
+                    File.AppendAllText(pathFileSalvato, stringa + Environment.NewLine);
+                }
+
+                MessageBox.Show("Salvataggio password avvenuto con successo, path : " + pathFileSalvato, "successo");
+            }
+
+        }
+    }
+}
